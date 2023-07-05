1601c65d824a0e12d3f67c5bd1b923f9959f8876
Giora Marani <giora.marani@gmail.com>
2023-07-05 09:31:38 +0300

    finish all features except sort type of rating

---
+++ cmps/RateBySelect.js
@@ -0,0 +1,27 @@
+export default {
+  name:'',
+  props: [],
+  template: `
+             <!-- <form @submit.prevent="$emit.submit-review" class="add-review">
+            <h2>Your review helps our fellow readers</h2>
+            <input type="text" v-model="review.name" placeholder="Full Name"/>
+            <select v-model.number="review.rating">
+              <option value="">Select a rating</option>
+              <option value="5">5</option>
+              <option value="4">4</option>
+              <option value="3">3</option>
+              <option value="2">2</option>
+              <option value="1">1</option>
+            </select>
+            <input type="date" v-model="review.readAt" placeholder="Full Name"/>
+            <button>Save</button>
+         </form> -->
+        `,
+created() {},
+  data() {
+    return {}
+  },
+  methods: {},
+  computed: {},
+components:{},
+}
